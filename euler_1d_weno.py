ac23d5b2da5d1fce0051aaf6b8b4e4e8a2bffdf5
Bryson Sullivan <btsllvn2@illinois.edu>
2018-05-02 14:18:49 -0500

    Evolution

--- euler_1d_weno.py
+++ euler_1d_weno.py
@@ -16,7 +16,7 @@ def init_cond(X_min,X_max,N,P4,T4,P1,T1,x_bnd=0.0):
 
     #initialize primative variables
     x = np.linspace(X_min,X_max,N)
-    for i in range(3,N-3):
+    for i in range(N):
         if (X[i]<=x_bnd):
             P[i] = P4
             T[i] = T4
@@ -96,9 +96,9 @@ def euler_1d_wavespeed(q):
 
 def proj_to_char(q,f,q_i_ip1):
     '''
-        q is a nsxnv matrix of conservative variables (ns = num pts in current stencil)  
-        f is a nsxnv matrix of conservative fluxes (ns = num pts in current stencil)  
-        q_i_ip1 is a 2xnv matrix with nv variables to compute average state 
+    q is a nsxnv matrix of conservative variables (ns = num pts in current stencil)  
+    f is a nsxnv matrix of conservative fluxes (ns = num pts in current stencil)  
+    q_i_ip1 is a 2xnv matrix with nv variables to compute average state 
 
     '''
     import numpy as np
@@ -136,8 +136,8 @@ def proj_to_char(q,f,q_i_ip1):
 
 def proj_to_cons(f_char,q_cons):
     '''
-        f_char is a Nxnv matrix of the characteristic flux for all grid points
-        q_cons is a Nxnv matrix of the conservative variables for all grid points
+    f_char is a Nxnv matrix of the characteristic flux for all grid points
+    q_cons is a Nxnv matrix of the conservative variables for all grid points
 
     '''
     import numpy as np
@@ -172,7 +172,7 @@ def proj_to_cons(f_char,q_cons):
 
     return f_cons
 
-def q1d_afunc(x,r,demo=False):
+def q1d_afunc(x,r,makePlot=False,demo=False):
 #computes 1/A*dA/dx based on a provided geometry R(x)
 
     from scipy.interpolate import splrep, splev
@@ -184,22 +184,33 @@ def q1d_afunc(x,r,demo=False):
     #constants
     pi = 4*np.arctan(1)
 
-    #demo procedure
-    if (demo):
-    
+    #defensive programming
+    for vec in (x,r):
+        vec = np.array(vec)
+    #if len(x) != len(r):
+    #    print('\tProvided xy-data is inconsistent! Exiting...\n')
+    #    sys.exit()
+
+    #demo inputs
+    if (makePlot):
+  
         # GALGIT Ludwieg tube geometry + Mach 5 nozzle
-        X = np.array([[-17.00000,  0.0568613],
-                      [-0.445792,  0.0568613],
-                      [0.0295679,  0.0568613],
-                      [0.0384761,  0.0369598],
-                      [0.0538287,  0.0233131],
-                      [0.0828279,  0.0159212],
-                      [0.131160,   0.0233131],
-                      [0.203942,   0.0363912],
-                      [0.292646,   0.0471948],
-                      [0.405800,   0.0557240],
-                      [0.543973,   0.0579985],
-                      [2.000000,   0.0579985]])
+        if (demo): 
+            X = np.array([[-17.00000,  0.0568613],
+                          [-0.445792,  0.0568613],
+                          [-0.100000,  0.0568613],
+                          [0.0295679,  0.0568613],
+                          [0.0384761,  0.0369598],
+                          [0.0538287,  0.0233131],
+                          [0.0828279,  0.0159212],  #<----throat location
+                          [0.131160,   0.0233131],
+                          [0.203942,   0.0363912],
+                          [0.292646,   0.0471948],
+                          [0.405800,   0.0557240],
+                          [0.543973,   0.0579985],
+                          [0.700000,   0.0579985],
+                          [2.000000,   0.0579985]])
+            x,r = X[:,0],X[:,1]
 
         #use LaTeX formatting for titles and axes
         plt.rc('text', usetex=True)
@@ -208,26 +219,20 @@ def q1d_afunc(x,r,demo=False):
 
         #plot the discrete points and the phcip spline
         plt.figure()
-        x,r = X[:,0],X[:,1]
         x_plt = np.linspace(x.min(),x.max(),int(1e4))
         r_func = interp.pchip(x,r)
-        plt.plot(x_plt,r_func.__call__(x_plt),'-b',linewidth=3.0,label='PCHIP Spline',zorder=1)
-        #r_func = interp.Akima1DInterpolator(x,r)
-        #plt.plot(x_plt,r_func.__call__(x_plt),'-b',linewidth=3.0,label='Akima Spline',zorder=1)
-        plt.scatter(x,r,c='r',label='Imported Data Points',zorder=2)
+        plt.plot(x_plt,r_func.__call__(x_plt),'-b',linewidth=3.0,label='PCHIP Interpolant',zorder=1)
+        plt.plot(x_plt,-r_func.__call__(x_plt),'-b',linewidth=3.0,zorder=2)
+        plt.scatter(x,r,c='r',label='Imported Data Points',zorder=3)
+        plt.scatter(x,-r,c='r',zorder=4)
         plt.plot([-1,3],[0,0],'-.k',linewidth=2,zorder=3)
-        plt.xlim(-0.1,0.75)
-        plt.ylim(-0.06,0.1)
+        plt.xlim(-0.1,0.7)
+        plt.ylim(-0.1,0.1)
         plt.xlabel(r'$x\;[m]$',fontsize=15)
         plt.ylabel(r'$r\;[m]$',fontsize=15)
-        plt.title(r'GALCIT Ludwieg Tube - Nozzle',fontsize=15)
-        plt.legend()
+        plt.title(r'GALCIT Ludwieg Tube - Nozzle',fontsize=17)
+        plt.legend(fontsize=12)
         plt.show()
-        sys.exit()
-
-    #defensive programming
-    if (len(x) != len(r)):
-        print('\tXY data is not consistent')
         sys.exit()
 
     #create an interpolant for log[A(x)] 
@@ -236,13 +241,226 @@ def q1d_afunc(x,r,demo=False):
     #output the derivative function
     return l_func.__call__(x,1)
 
+
+def Shock_Tube_Exact(q_L,q_R,x,t,makePlots=False):
+    '''
+    ================================================================
+                                                                    
+       Function which computes the analytical solution              
+       to the 1D shock-tube problem given left and right            
+       initial conditions (assumes no shock reflections)            
+                                                                    
+       Usage: q_an = Shock_Tube_Exact_ND(q_L,q_R,x,t,n_plot), where 
+                                                                    
+       q_an = Nx3 array of the analytical solution q(x) at time t   
+       q_L  = 3x1 vector of the initial driver section conditions   
+       q_R  = 3x1 vector of the initial driven section conditions   
+       x    = Nx1 vector of x-locations of computatonal gridpoints               
+       t    = current solution time [s]                             
+                                                                    
+    ================================================================
+    
+    '''
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import sys, os
+
+    #define constants
+    eps = np.finfo(float).eps
+    pi = np.pi
+    gam = 1.4
+    R = 286.9
+    alpha  = (gam+1)/(gam-1)
+    f_num = 1 
+
+    #verify input arrays
+    var_list = [q_L, q_R, x]
+    for var in var_list:
+        var = np.array(var)
+
+    #normal operation or demo mode
+    if len(sys.argv) == 0:
+
+        #example initial conditions (Sod '78)
+        q_L = np.array([1, 0, 10**5/(gam-1)])
+        q_R = np.array([0.125, 0, 10**4/(gam-1)])
+
+        #set domain limits
+        x_min = 0.0; x_max = 10.0
+        x = np.linspace(x_min,x_max,501)
+        x0 = (max(x)+min(x))/2
+        t = 0.0061
+
+    elif len(sys.argv) == 4:
+
+        #set domain limits for provided x-vector
+        x_min,x_max = min(x),max(x)
+        x0 = 0
+
+    else:
+
+        print('\tUnrecognized number of inputs! Exiting...\n')
+        sys.exit()
+
+    #total number of points on the grid
+    N = x.shape[0] 
+
+    #compute the acoustic speeds in the left- and right-states
+    c_L = np.sqrt(gam*(gam-1)*q_L[2]/q_L[0])
+    c_R = np.sqrt(gam*(gam-1)*q_R[2]/q_R[0])
+
+    #compute the shock Mach number Ms using Newton-Raphson + Complex-Step Derivative
+    P_41 = q_L[2]/q_R[2]
+    err = 1.0; cntr = 0; h=1e-30; Ms = 1.5
+    Res = lambda M_sh: P_41*((gam-1)/(gam+1)*(c_R/c_L)*(M_sh**2-1)/M_sh-1)**(2*gam/(gam-1))+2*gam/(gam+1)*(M_sh**2-1)+1
+    print('Solving for shock Mach number (Ms) based on PR = P_l/P_r:')
+    while (err>eps): 
+        cntr += 1; Msh_p = M_sh
+        M_sh -= h*Res(M_sh)/np.imag(Res(complex(M_sh,h)))
+        err = Ms/Msh_p-1
+        print('    N = %d, e = %1.6e\n' % (cntr,err))
+
+        #limit total number of iterations
+        if (cntr == 15):
+            print('\n  **Iteration limit exceeded (e = %1.6e)\n' % err)
+            break
+
+    #compute shock velocity [m/s] and static pressure-jump P_21 = P2/P1
+    v_sh = M_sh*c_R
+    x_sh = x0 + v_sh*t
+    P_21 = 1.0+2*gam/(gam-1)*(M_sh**2-1)
+    if len(sys.argv) == 0:
+        print('\n\n\tM_shock = %2.3f\n\n' % M_sh)
+
+    #compute velocity of the contact discontinuity [m/s]
+    v_ct = 2*c_L/(gam-1)*(1-(P_21/P_41)**((gam-1)/(2*gam)))
+    x_ct = x0 + v_ct*t
+    u3 = v_ct; u2 = v_ct
+
+    #compute sound speeds in Regions 2 and 3
+    c2 = c_R*np.sqrt(P_21*(2+(gam-1)*M_sh**2)/((gam+1)*M_sh**2))
+    c3 = c_L*(P_21/P_41)**((gam-1)/(2*gam))
+
+    #compute velocity of left and right sides of the expansion fan
+    v_fL = -c_L
+    x_fL = x0 + v_fL*t
+    v_fR = u2-c3
+    x_fR = x0 + v_fR*t
+
+    #write out the solution
+    q_an = np.zeros((N,3))
+    for i in range(N):
+
+
+        #undisturbed driven (right) state 
+        if (x[i]>x_sh):
+            rho = q_R[0]
+            u   = 0.0
+            p   = (gam-1)*q_R[2]
+
+        #undisturbed driver (left) state
+        elif (x[i]<x_fL):
+            rho = q_L[0]
+            u   = 0.0
+            p   = (gam-1)*q_L[2]
+
+        #between the head of the expansion fan the the shock
+        else:
+
+            #define a locator variable
+            phi = (x[i]-x_fR)/(x_ct-x_fR)
+            
+            #x between shock and contact discontinuity
+            if (phi>1.0):
+                rho = ((1+alpha*P_21)/(alpha+P_21))*q_R[0]
+                u   = u2
+                p   = P_21*((gam-1)*q_R[2])
+
+            #x within the expansion fan
+            elif (phi<0.0):
+                u   = 2/(gam+1)*(c_L+(x[i]-x0)/t)
+                p   = (gam-1)*q_L[2]*(1-(gam-1)*u/(2*c_L))**(2*gam/(gam-1))
+                rho = q_L[0]*(p/((gam-1)*q_L[2]))**(1/gam)
+
+            #x between contact disconinuity and expansion fan
+            else:
+                rho = q_L[0]*(P_21/P_21)**(1/gam)
+                u   = u3
+                p   = P_21*((gam-1)*q_R[2])
+
+        #construct solution vector from the primitive variables
+        q_an[i,0] = rho
+        if (abs(t)>eps):
+            q_an[i,1] = rho*u
+        q_an[i,2] = p/(gam-1)+0.5*rho*u**2
+
+
+    #plot the solution if run without arguments 
+    if len(sys.argv) == 0:
+
+        #compute the variables
+        P_plot = (gam-1)*(q_an[:,2]-q_an[:,1]**2/(2*q_an[:,0]))
+        U = q_an[:,1]/q_an[:,0]
+        Mach = np.sqrt(q_an[:,0]*U**2/(gam*P_plot))
+        T_plot = P_plot/(R*q_an[:,0])
+        c_plot = np.sqrt(gam*R*T_plot)
+        Entropy = P_plot/(q_an[:,0])**(gam)
+        e = P_plot/(gam-1)+0.5*U**2
+        str_t = 'Sod''s Shock Tube Problem (t=%1.2f)' % t
+
+        #make the figures
+        plt.clf()
+        fig = plt.figure(f_num)
+        ax1 = fig.add_subplot(221)
+        ax1.plot(x,q_an[:,0],'-b',linewidth=3.0)
+        ax1.set_title(str_t)
+        ax1.grid()
+        ax1.xlabel(r'$x$')
+        ax1.ylabel(r'$\rho$')
+        ax1.xlim(x_min,x_max)
+        #ax1.xaxis.tick_bottom()
+        ax2 = fig.add_subplot(222)
+        ax2.plot(x,P_plot,'-b',linewidth=3.0)
+        ax2.set_title(str_t)
+        ax2.grid()
+        ax2.xlabel(r'$x$')
+        ax2.ylabel(r'$P$')
+        ax2.xlim(x_min,x_max)
+        #ax2.xaxis.tick_bottom()
+        ax3 = fig.add_subplot(223)
+        ax3.plot(x,U,'-b',linewidth=3.0)
+        ax3.set_title(str_t)
+        ax3.grid()
+        ax3.xlabel(r'$x$')
+        ax3.ylabel(r'$U$')
+        ax3.xlim(x_min,x_max)
+        #ax3.xaxis.tick_bottom()
+        ax4 = fig.add_subplot(224)
+        ax4.plot(x,Mach,'-b',linewidth=3.0)
+        ax4.set_title(str_t)
+        ax4.grid()
+        ax4.xlabel(r'$x$')
+        ax4.ylabel(r'Mach')
+        ax4.xlim(x_min,x_max)
+        #ax4.xaxis.tick_bottom()
+        fig.tight_layout()
+        plt.savefig('fig_%d.pdf' % f_num)
+        f_num += 1
+
+        #show the plot(s)
+        plt.show()
+     
+    return q_an
+
+
 def phi_weno5():
     '''
-        Function which computes the 5th order WENO reconstruction for the flux at x_{i+1/2}
+    Function which computes the 5th order WENO reconstruction for the flux at x_{i+1/2}
 
-        q is a nsxnv matrix of conservative variables (ns = num pts in current stencil)  
-        f is a nsxnv matrix of conservative fluxes (ns = num pts in current stencil)  
-        q_i_ip1 is a 2xnv matrix with nv variables to compute average state 
+    q is a nsxnv matrix of conservative variables (ns = num pts in current stencil)  
+    f is a nsxnv matrix of conservative fluxes (ns = num pts in current stencil)  
+    q_i_ip1 is a 2xnv matrix with nv variables to compute average state 
 
     '''
 
@@ -252,4 +470,4 @@ def phi_weno5():
 
 #attempt to plot the shoc tube geometry
 import numpy as np
-q1d_afunc(1,1,True)
+q1d_afunc(1,1,True,True)
