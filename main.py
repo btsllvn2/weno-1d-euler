b81b0851459a24521fd68faff17fe295992f4fa3
Bryson Sullivan <btsllvn2@illinois.edu>
2018-05-05 13:16:44 -0500

    Added XT-plots for primitive variables

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ import sys,os
 #  Main options for running the code:
 #
 #========================================
-noDisplay = False
-saveFrames = False
-runQuasi1D = False
-plot_freq = 1
+runQuasi1D = False      # Run with the Q1D rhs source term
+saveFrames = False      # Frames from solution are saved to disk
+noDisplay  = False      # All realtime plotting is supressed 
+plot_freq  = 1          # Frequency of making/saving plots
 
 #supress display output
 if (noDisplay):
@@ -32,6 +32,7 @@ if (noDisplay):
     matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib import cm
 
 #create a 'frames' directory if one does not already exist
 if not os.path.exists('frames'):
@@ -48,14 +49,14 @@ gam = 1.4
 R = 286.9
 
 # Specify the number of points in the domain (includes ghost points)
-N = 1500
+N = 200
 
 # Specify the overall domain size
-X_min,X_max = -10.0,1.0
+X_min,X_max = -1.0,1.0
 
 # Specifiy target CFL and total number of time steps
 CFL = 0.5
-Nt = 1200
+Nt = 500
 
 # Specify the Pressure and temperature values of the initial condition
 P1,P4,T1,T4 = 1e4,1e5,300,300
@@ -123,7 +124,6 @@ else:
     #force the Q1D source term to be identically zero
     F_vec = np.zeros(X[3:-3].shape)
 
-
 print('Maximum possible velocity on domain: u_max = %4.3f [m/s]' % u_max)
 P_41 = int(P4/P1)
 
@@ -132,6 +132,8 @@ q = np.copy(q_init)
 Q = np.zeros((q.shape[0]-6,q.shape[1],Nt+1))            #<-stored history
 Q[:,:,0] = q_init[3:-3,:]
 q1,q2 = np.zeros(q.shape),np.zeros(q.shape)
+x_vec = X[3:-3]
+t_vec = np.linspace(0.0,Nt*dt,Nt+1)
 
 rho_plt = q[3:-3,0]
 u_plt = q[3:-3,1]/rho_plt
@@ -143,26 +145,26 @@ M_plt = np.sqrt(rho_plt*u_plt**2/(gam*p_plt))
 plt.ion()
 plt.figure()
 if(runQuasi1D):
-    plt.title('Solution to GALCIT Nozzle Flow Using WENO-JS ($P_{41}$=%d, t=%2.3f[ms])' %(P_41,0.0))
+    plt.title('Solution to GALCIT Nozzle Flow Using WENO-JS ($P_{41}$=%d, t=%2.3f[ms])' %(P_41,0.0),fontsize=15)
     line1, = plt.plot(X[3:N-3],Mach_vec,'--k',label='Isentropic Solution',linewidth=1.0)
     line2, = plt.plot(X[3:N-3],M_plt,'-b',label='WENO-JS',linewidth=3.0)
-    plt.legend(loc=2)
-    plt.ylabel('Mach')
+    plt.legend(loc=2,fontsize=12)
+    plt.ylabel('Mach',fontsize=15)
     plt.xlim(-0.1,0.7)
     plt.ylim(0,5.0)
 else:
     line1, = plt.plot(X[3:N-3],Q_exact[3:N-3,0,0],'-k',linewidth=1.0,label='Exact Solution')
     line2, = plt.plot(X[3:N-3],q_init[3:N-3,0],'ob',label='WENO-JS')
-    plt.title('1D Euler Equations Using WENO-JS ($P_{41}$=%d, t=%2.3f[ms])' % (P_41,0.0))
-    plt.ylabel('rho')
+    plt.title("Sod's Shock Tube Problem ($P_{41}$=%d, t=%2.3f[ms])" % (P_41,0.0),fontsize=15)
+    plt.ylabel('Density',fontsize=15)
     plt.xlim(-1,1)
-    plt.legend()
-plt.xlabel('x[m]')
+    plt.legend(fontsize=12)
+plt.xlabel(r'$x$[m]',fontsize=15)
 plt.draw()
 plt.savefig('frames/frame%08d.png' % 0)
 plt.pause(eps)
 
-#time integration
+#perform time integration
 print('\nStarting time integration...')
 for i in range(1,Nt+1):
 
@@ -195,15 +197,56 @@ for i in range(1,Nt+1):
     #real-time animation   
     if(i%plot_freq==0):
         if(runQuasi1D): 
-            plt.title('Solution to GALCIT Nozzle Flow Using WENO-JS ($P_{41}$=%d, t=%2.3f[ms])' % (P_41,float(1000*i*dt)))
+            plt.title('Solution to GALCIT Nozzle Flow Using WENO-JS ($P_{41}$=%d, t=%2.3f[ms])' % (P_41,float(1000*i*dt)),fontsize=15)
             line2.set_ydata(M_plt)
         else:
             line1.set_ydata(Q_exact[3:N-3,0,i])
             line2.set_ydata(q[3:N-3,0])
-            plt.title('1D Euler Equations Using WENO-JS ($P_{41}$=%d, t=%2.3f[ms])' % (P_41,float(1000*i*dt)))
+            plt.title("Sod's Shock Tube Problem ($P_{41}$=%d, t=%2.3f[ms])" % (P_41,float(1000*i*dt)),fontsize=15)
         if (saveFrames): plt.savefig('frames/frame%08d.png' % int(i/plot_freq))
         plt.pause(eps)
 
-print('\nProgram complete.\n')
+
+#compute XT-flow variables for plotting
 plt.ioff()
+R = 286.9
+RHO = Q[:,0,:].T
+U = Q[:,1,:].T/RHO
+E = Q[:,2,:].T
+P = (gam-1)*(E-0.5*RHO*U**2)
+M = np.sqrt(RHO*U**2/(gam*P))
+ENTROPY = P/RHO**gam
+T = P/(R*RHO)
+
+#title options
+if (runQuasi1D):
+    title_str = r'GALCIT Ludwieg Tube ($P_{41}$=%d)' % P_41
+else:
+    title_str = r"Sod's Shock Tube Problem Tube ($P_{41}$=%d)" % P_41
+
+#generate XT-plots
+f_num = 3
+X,T = np.meshgrid(x_vec,1e3*t_vec)
+def make_XT_plot(var,v_label):
+
+    global f_num
+
+    plt.figure(f_num)
+    plt.contourf(X,T,var,300,cmap='jet')
+    plt.title(title_str,fontsize=12)
+    plt.xlabel(r'$x$ [m]',fontsize=12)
+    plt.ylabel(r'Time [ms]',fontsize=12)
+    cb = plt.colorbar()
+    cb.set_label(v_label, labelpad=20, rotation=-90)
+    plt.savefig('fig_%d.png' % f_num)
+    f_num += 1
+
+    return
+
+#make an xt-plot for each variable
+var_lst = [RHO,P,T,M,E,ENTROPY]
+label_lst = [r'Density [kg/m^3]',r'Pressure [Pa]',r'Temperature [K]',r'Mach [-]',r'Specific Energy',r'Measure of Entropy']
+for i in range(len(var_lst)): make_XT_plot(var_lst[i],label_lst[i])
+
+print('\nProgram complete.\n')
 plt.show()
